ca1aba18a25e5da7d82ecae1e51cbd80c97fdaf8
Siddu <siddusthawarmath@gmail.com>
2019-02-25 14:38:06 +0530

    Custom hashset implemenatation

--- src/main/java/com/siddu/hashset/CustomHashSet.java
+++ src/main/java/com/siddu/hashset/CustomHashSet.java
@@ -1,37 +1,52 @@
 package com.siddu.hashset;
 
-import javax.swing.text.Element;
-
-import com.siddu.hashmap.CustomHashMap;
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class CustomHashSet {
+	private transient HashMap map;
 
-	int CAPACITY = 10;
-
-	CustomHashMap customHashMap = null;
-
-	final Object DUMMY_OBJECT = new Object();
+	private static final Object PRESENT = new Object();
 
 	public CustomHashSet() {
-		customHashMap = new CustomHashMap();
+		map = new HashMap(10);
 	}
 
-	public void add(Object element) {
-		customHashMap.put(element, DUMMY_OBJECT);
+	public boolean add(Object key) {
+		System.out.println(key + " added element to set");
+		return map.put(key, PRESENT) == null;
 	}
 
-	/*public Object get(Object element) {
-		return customHashMap.get(element);
+	public Object remove(Object key) {
+		System.out.println("\n" + key + " removed element from set");
+		return map.remove(key);
 	}
-	 */
-	
-	
+
+	public Object get(Object e) {
+		return map.get(e);
+	}
+
+	public Iterator iterator() {
+		return map.keySet().iterator();
+	}
+
+	public void display() {
+		System.out.println("\nAll elements from the set");
+		Iterator iterator = iterator();
+		while (iterator.hasNext()) {
+			System.out.print(iterator.next() + "\t");
+		}
+	}
+
 	public static void main(String[] args) {
-		
-		CustomHashSet customHashSet=new CustomHashSet();
-		customHashSet.add(10);
+		CustomHashSet customHashSet = new CustomHashSet();
 		customHashSet.add(10);
 		customHashSet.add(20);
-		
+		customHashSet.display();
+
+		Object ob = customHashSet.get(0);
+		System.out.println(ob);
+		customHashSet.remove(10);
+		customHashSet.display();
 	}
 }
